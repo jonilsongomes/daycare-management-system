a846ccefb42f865c3e9b60ee8a561f9789e6c4fa
Janilson da Silva <jonilsongomes@gmail.com>
2024-09-10 21:06:11 +0100

    Adicionar a app Spring

---
+++ daycare-management-system/src/main/java/em/dio/daycare_management_system/DaycareManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package em.dio.daycare_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaycareManagementSystemApplication.class, args);
+	}
+
+}
---
+++ daycare-management-system/src/test/java/em/dio/daycare_management_system/DaycareManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package em.dio.daycare_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaycareManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
